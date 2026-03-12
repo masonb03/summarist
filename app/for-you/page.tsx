4af76d92f95834530eb6ada2880bcb410e773041
Mason <bevinsm355@gmail.com>
2026-03-11 20:33:14 -0400

    Add initial components and styles for the application layout

---
+++ app/for-you/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>For you Page</div>
+  )
+}
+
+export default page
---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../public/logo.png'
+import Image from 'next/image'
+import styles from '../styles/navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.nav__wrapper}>
+        <figure className={`$styles.nav__img--mask`}>
+          <Image className={styles.nav__img} src={logo} alt="logo" />
+        </figure>
+        <ul className={`$styles.nav__list--wrapper`}>
+          <li className={styles.nav__list}>Login</li>
+          <li className={styles.nav__list}>About</li>
+          <li className={styles.nav__list}>Contact</li>
+          <li className={styles.nav__list} >Help</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
